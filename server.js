30f19bebab082594575a79fcdf81b9c17f5638d0
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2014-11-15 21:51:39 +0100

    Store game properties directly in the socket object

--- server.js
+++ server.js
@@ -18,16 +18,13 @@ var games = {};
 
 function handleGame (game) {
   var gameID = used.shift() || ++gameCounter;
-  var g = games[gameID] = {
-    'id': gameID,
-    'ws': game,
-    'count': 0,
-    'players': {}
-  };
+  game.gameID = gameID;
+  game.count = 0;
+  game.players = {};
 
   game.on('disconnect', function () {
-    Object.keys(g.players).forEach(function (player) {
-      g.players[player].emit('disconnect');
+    Object.keys(game.players).forEach(function (player) {
+      game.players[player].emit('disconnect');
     });
     used.push(gameID);
     delete games[gameID];
@@ -35,43 +32,43 @@ function handleGame (game) {
 
   game.on('updatePlayer', function (msg) {
     var toPlayer = msg.playerID;
-    if (g.players[toPlayer]) {
-      g.players[toPlayer].emit('message', msg);
+    if (game.players[toPlayer]) {
+      game.players[toPlayer].emit('message', msg);
     }
   });
 
   game.on('broadcast', function (msg) {
-    Object.keys(g.players).forEach(function (p) {
-      g.players[p].emit('message', msg);
+    Object.keys(game.players).forEach(function (p) {
+      game.players[p].emit('message', msg);
     });
   });
 
-  game.emit('gameID', gameID);
+  games[gameID] = game;
 
+  game.emit('gameID', gameID);
 }
 
 function handlePlayer (player) {
   player.on('join', function (gameID) {
     if (!games[gameID]) {
-      return player.emit('join', { 'error': 'This game session does not exist' });
+      return player.emit('joined', { 'error': 'This game session does not exist' });
     }
 
     var g = games[gameID];
     if (player.hasOwnProperty('gameID')) {
-      // player is already in a game
-      return;
+      return player.emit('joined', { 'error': 'The player is already part of game ' + player.gameID });;
     }
 
     player.gameID = gameID;
     player.id = g.count++;
     g.players[player.id] = player;
-    g.ws.emit('addPlayer', { 'playerID': player.id });
+    g.emit('addPlayer', { 'playerID': player.id });
     player.emit('message', { 'cmd': 'joined'});
   });
 
   player.on('disconnect', function () {
     if (player.gameID && games[player.gameID]) {
-      games[player.gameID].ws.emit('removePlayer', { 'playerID': player.id });
+      games[player.gameID].emit('removePlayer', { 'playerID': player.id });
       delete games[player.gameID].players[player.id];
     }
   });
@@ -79,7 +76,7 @@ function handlePlayer (player) {
   player.on('update', function (msg) {
     if (player.gameID && games[player.gameID]) {
       msg.playerID = player.id;
-      games[player.gameID].ws.emit('updatePlayer', msg);
+      games[player.gameID].emit('updatePlayer', msg);
     }
   });
 
